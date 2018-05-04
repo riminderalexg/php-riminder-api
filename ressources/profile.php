6336678096510885add8ccdaa35fb011b0dac1ef
Alexandre Gotte <alexandre.gotte@riminder.net>
2018-05-04 14:32:32 +0200

    Added DateTime support for profile methods

--- ressources/profile.php
+++ ressources/profile.php
@@ -6,7 +6,7 @@
       $this->riminder = $parent;
     }
 
-    public function serializeSourceIds($source_ids) {
+    private static function serializeSourceIds($source_ids) {
       $res = "";
       $i = 0;
       foreach ($source_ids as $source_id) {
@@ -17,13 +17,25 @@
       }
       return($res);
     }
+
+    private static function dateToTimestamp($date) {
+
+      if (is_int($date)){
+        return $date;
+      }
+      if ($date != new DateTime()) {
+        return null;
+      }
+      return $date->getTimestamp();
+    }
+
     public function getProfiles($source_ids, $date_start, $date_end, $page = 1, $limit = null, $sort_by = null, $seniority = null, $job_id = null, $stage = null) {
 
 
       $query = array (
-        'source_ids'  => $this->serializeSourceIds($source_ids),
-        'date_start'  => $date_start,
-        'date_end'    => $date_end,
+        'source_ids'  => RiminderProfile::serializeSourceIds($source_ids),
+        'date_start'  => RiminderProfile::dateToTimestamp($date_start),
+        'date_end'    => RiminderProfile::dateToTimestamp($date_end),
       );
       if ($page != null) {
         $query['page'] = $page;
@@ -48,12 +60,12 @@
       return $resp->decode_response();
     }
 
-    public function add($source_id, $file, $profile_reference, $timestamp_reception) {
+    public function add($source_id, $file, $profile_reference, $reception_date) {
       $bodyParams = array (
         'source_id'           => $source_id,
         'file'                => $file,
         'profile_reference'   => $profile_reference,
-        'timestamp_reception' => $timestamp_reception
+        'timestamp_reception' => RiminderProfile::dateToTimestamp($reception_date)
       );
       $resp = $this->riminder->_rest->post("profile", $bodyParams);
       return $resp->decode_response();
